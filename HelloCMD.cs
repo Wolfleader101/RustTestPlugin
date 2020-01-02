e96106aff6b46ea4e6fa5d514749605752ade353
Wolfleader101 <james.morgan@iinet.net.au>
2020-01-02 11:19:11 +0800

    Added comment

--- HelloCMD.cs
+++ HelloCMD.cs
@@ -8,19 +8,20 @@ namespace Oxide.Plugins {
         [ChatCommand("hi")]
         void HelloCmd(BasePlayer player) {
             sendMessage(player, "Hello, " + player.displayName);
+            Puts("Hello Command was fired by: " +player.displayName);
 
         }
         void sendMessage(BasePlayer player, string msg, params object[] args) {
             PrintToChat(player, msg, args);
         }
 
+        // Use this to send msg to all players on server
         void Broadcast(string msg, params object[] args) {
             PrintToChat(msg, args);
         }
 
         void Loaded() {
-
-            PrintToChat("The plugin has been loaded");
+            PrintToChat("Hello Command Plugin has been Loaded");
 
         }
     }
